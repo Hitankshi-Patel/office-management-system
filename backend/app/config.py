f44069e4a9b2f1bb53d8b65a2921c25c310f21a3
Hitankshi Patel <hitankshipatel36@gmail.com>
2026-05-20 15:43:34 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
+    ALLOWED_ORIGINS: str = "http://localhost:5173,http://localhost:3000,https://office-management-system-five.vercel.app"
 
     @property
     def get_database_url(self) -> str:
